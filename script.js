a7a6bf14ebbea081dea485e82133082cdd4c8c58
waqas saleem <waqas.mba86@gmail.com>
2020-01-23 07:48:11 -0800

    my first commit of lucky draw

---
+++ script.js
@@ -0,0 +1,214 @@
+var persons = [];
+var num = 0;
+
+btn1.onclick = function(){
+var num = 1;
+div_notification.innerHTML = "";
+div_list.innerHTML = "";
+span1.innerHTML = "";
+persons.push("WAQAS SALEEM");
+document.getElementById("div_notification").className = "div_noti"
+document.getElementById("div_list").className = "divlist"
+div_notification.innerHTML = "Congratulations...! Mr. "+" WAQAS SALEEM";
+
+for(var i=0; i<persons.length; i++){
+div_list.innerHTML +=  num++  + "-" + persons[i] + "<br>";
+
+};
+};
+
+
+
+
+btn2.onclick = function(){
+var num = 1;
+div_notification.innerHTML = "";
+div_list.innerHTML = "";
+span2.innerHTML = "";
+persons.push("FAISAL YASEEN");
+document.getElementById("div_notification").className = "div_noti2"
+document.getElementById("div_list").className = "divlist"
+div_notification.innerHTML = "Congratulations...! Mr. FAISAL YASEEN";
+
+for(var i=0; i<persons.length; i++){
+div_list.innerHTML +=  num++  + "-" + persons[i] + "<br>";
+
+};
+};
+
+
+
+btn3.onclick = function(){
+var num = 1;
+div_notification.innerHTML = "";
+div_list.innerHTML = "";
+span3.innerHTML = "";
+persons.push("SALMAN AHMAD");
+document.getElementById("div_notification").className = "div_noti3"
+document.getElementById("div_list").className = "divlist"
+div_notification.innerHTML = "Congratulations...! Mr. SALMAN AHMAD";
+
+for(var i=0; i<persons.length; i++){
+div_list.innerHTML +=  num++  + "-" + persons[i] + "<br>";
+
+};
+};
+
+
+btn4.onclick = function(){
+var num = 1;
+div_notification.innerHTML = "";
+div_list.innerHTML = "";
+span4.innerHTML = "";
+persons.push("SHAMSHAD AHMAD");
+document.getElementById("div_notification").className = "div_noti4"
+document.getElementById("div_list").className = "divlist"
+div_notification.innerHTML = "Congratulations...! Mr. SHAMSHAD AHMAD";
+
+for(var i=0; i<persons.length; i++){
+div_list.innerHTML +=  num++  + "-" + persons[i] + "<br>";
+
+};
+};
+
+
+btn5.onclick = function(){
+var num = 1;
+div_notification.innerHTML = "";
+div_list.innerHTML = "";
+span5.innerHTML = "";
+persons.push("KASHIF MUNIR");
+document.getElementById("div_notification").className = "div_noti5"
+document.getElementById("div_list").className = "divlist"
+div_notification.innerHTML = "Congratulations...! Mr. KASHIF MUNIR";
+
+for(var i=0; i<persons.length; i++){
+div_list.innerHTML +=  num++  + "-" + persons[i] + "<br>";
+
+};
+};
+
+
+
+btn6.onclick = function(){
+var num = 1;
+div_notification.innerHTML = "";
+div_list.innerHTML = "";
+span6.innerHTML = "";
+persons.push("ZEESHAN AHMAD");
+document.getElementById("div_notification").className = "div_noti6"
+document.getElementById("div_list").className = "divlist"
+div_notification.innerHTML = "Congratulations...! Mr. ZEESHAN AHMAD";
+
+for(var i=0; i<persons.length; i++){
+div_list.innerHTML +=  num++  + "-" + persons[i] + "<br>";
+
+};
+};
+
+
+
+btn7.onclick = function(){
+var num = 1;
+div_notification.innerHTML = "";
+div_list.innerHTML = "";
+span7.innerHTML = "";
+persons.push("MUHAMMAD NOMAN");
+document.getElementById("div_notification").className = "div_noti7"
+document.getElementById("div_list").className = "divlist"
+div_notification.innerHTML = "Congratulations...! Mr. MUHAMMAD NOMAN";
+
+for(var i=0; i<persons.length; i++){
+div_list.innerHTML +=  num++  + "-" + persons[i] + "<br>";
+
+};
+};
+
+
+
+btn8.onclick = function(){
+var num = 1;
+div_notification.innerHTML = "";
+div_list.innerHTML = "";
+span8.innerHTML = "";
+persons.push("ABDUL MANAN");
+document.getElementById("div_notification").className = "div_noti8"
+document.getElementById("div_list").className = "divlist"
+div_notification.innerHTML = "Congratulations...! Mr. ABDUL MANAN";
+
+for(var i=0; i<persons.length; i++){
+div_list.innerHTML +=  num++  + "-" + persons[i] + "<br>";
+
+};
+};
+
+
+
+btn9.onclick = function(){
+var num = 1;
+div_notification.innerHTML = "";
+div_list.innerHTML = "";
+span9.innerHTML = "";
+persons.push("ABDULLAH ANWAR");
+document.getElementById("div_notification").className = "div_noti9"
+document.getElementById("div_list").className = "divlist"
+div_notification.innerHTML = "Congratulations...! Mr. ABDULLAH ANWAR";
+
+for(var i=0; i<persons.length; i++){
+div_list.innerHTML +=  num++  + "-" + persons[i] + "<br>";
+
+};
+};
+
+
+
+btn10.onclick = function(){
+var num = 1;
+div_notification.innerHTML = "";
+div_list.innerHTML = "";
+span10.innerHTML = "";
+persons.push("GHULAM ABBAS");
+document.getElementById("div_notification").className = "div_noti10"
+document.getElementById("div_list").className = "divlist"
+div_notification.innerHTML = "Congratulations...! Mr. GHULAM ABBAS";
+
+for(var i=0; i<persons.length; i++){
+div_list.innerHTML +=  num++  + "-" + persons[i] + "<br>";
+
+};
+};
+
+
+btn11.onclick = function(){
+var num = 1;
+div_notification.innerHTML = "";
+div_list.innerHTML = "";
+span11.innerHTML = "";
+persons.push("SAWAR HUSSAIN");
+document.getElementById("div_notification").className = "div_noti11"
+document.getElementById("div_list").className = "divlist"
+div_notification.innerHTML = "Congratulations...! Mr. SAWAR HUSSAIN";
+
+for(var i=0; i<persons.length; i++){
+div_list.innerHTML +=  num++  + "-" + persons[i] + "<br>";
+
+};
+};
+
+
+
+btn12.onclick = function(){
+var num = 1;
+div_notification.innerHTML = "";
+div_list.innerHTML = "";
+span12.innerHTML = "";
+persons.push("ARMAN ALI");
+document.getElementById("div_notification").className = "div_noti12"
+document.getElementById("div_list").className = "divlist"
+div_notification.innerHTML = "Congratulations...! Mr. ARMAN ALI";
+
+for(var i=0; i<persons.length; i++){
+div_list.innerHTML +=  num++  + "-" + persons[i] + "<br>";
+
+};
+};
